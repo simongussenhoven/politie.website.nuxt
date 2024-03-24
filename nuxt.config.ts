9733a5b6890a270c2ad2dd15c5635d34aef621f4
Simon Gussenhoven <simon.gussenhoven@pidz.nl>
2024-03-24 01:21:49 +0100

    Add page transitioin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   runtimeConfig: {
     // Will be available at runtime
     public: {
